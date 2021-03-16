70fb272664d5adaeb09f2ab69b89298c93b13362
User <User@LAPTOP-Q75VBQEB>
2021-03-16 09:49:23 +0900

    zxzx

--- 8주차/bj_16174/Main_16174_유시준.java
+++ 8주차/bj_16174/Main_16174_유시준.java
@@ -8,7 +8,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main { 
+public class Main_16174_유시준 { 
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer st;
