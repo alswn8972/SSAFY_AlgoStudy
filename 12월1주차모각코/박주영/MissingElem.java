813b916b60cf1185c35762ea95048f0a0893f658
박주영 <zu0p@kakao.com>
2021-12-05 13:36:56 +0900

    Codility_FindMissingElement

--- 12월1주차모각코/박주영/MissingElem.java
+++ 12월1주차모각코/박주영/MissingElem.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class MissingElem {
+import java.util.*;
+
+class MissingElem {
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        int res = 0;
+        Arrays.sort(A);
+        int max = A.length+2;
+        boolean[] check = new boolean[max];
+        for(int i: A){
+            check[i] = true;
+        }
+        for(int i = 1; i<max; i++){
+            if(check[i])continue;
+            res = i;
+            break;
+        }
+
+        return res;
+    }
 }
